ad25e05a2193e62df0d4b0c3fd8a93f9f4922a83
Alberto Cohen <143648228+AlbertoCohen@users.noreply.github.com>
2024-09-10 17:52:22 -0300

    Add files via upload

---
+++ Partica productor/Consumidor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANTIDAD_PARTIDA 10
+#define INTERVALO_PRODUCTO 500
+#define INTERVALO_PARTIDA 1000
+#define LARGO 100
+
+int main()
+{
+	char cadena[LARGO]; /* Un array lo suficientemente grande como para guardar la línea más larga del fichero */
+	int cant_producto=0;
+	FILE *consumidor; 
+
+	while(1)
+	{
+		consumidor = fopen("producto.txt", "r");
+		if (consumidor!=NULL)
+		{
+			printf("\nCONSUMIMOS\n");
+			while (!feof(consumidor))
+			{
+				fscanf(consumidor,"%s",cadena);
+				printf("%s\n",cadena);
+				usleep(INTERVALO_PRODUCTO*1000);
+				cant_producto++;
+			}
+			fclose(consumidor);
+			if(cant_producto>CANTIDAD_PARTIDA)
+			{
+				printf("\nBORRAMOS\n");
+				remove("producto.txt");
+				cant_producto=0;
+			}
+			else
+				printf("\nNADA\n");
+		}
+		else
+			perror ("Error al abrir producto.txt");
+		printf("\nESPERAMOS\n");
+		usleep(INTERVALO_PARTIDA*1000);
+	};
+	return 0;
+}
---
+++ framework/archivos.c
@@ -0,0 +1,97 @@
+#include "archivos.h"
+
+FILE* abrirArchivoW(const char *nombre) {
+    FILE *fp = fopen(nombre, "w");
+    if (fp == NULL) manejarError("Error al abrir archivo para escritura.");
+    return fp;
+}
+
+FILE* abrirArchivoR(const char *nombre) {
+    FILE *fp = fopen(nombre, "r");
+    if (fp == NULL) manejarError("Error al abrir archivo para lectura.");
+    return fp;
+}
+
+FILE* abrirArchivoA(const char *nombre) {
+    FILE *fp = fopen(nombre, "a");
+    if (fp == NULL) manejarError("Error al abrir archivo para agregar.");
+    return fp;
+}
+
+FILE* abrirArchivoWPlus(const char *nombre) {
+    FILE *fp = fopen(nombre, "w+");
+    if (fp == NULL) manejarError("Error al abrir archivo para escritura.");
+    return fp;
+}
+
+FILE* abrirArchivoRPlus(const char *nombre) {
+    FILE *fp = fopen(nombre, "r+");
+    if (fp == NULL) manejarError("Error al abrir archivo para escritura.");
+    return fp;
+}
+
+FILE* abrirArchivoAPlus(const char *nombre) {
+    FILE *fp = fopen(nombre, "a+");
+    if (fp == NULL) manejarError("Error al abrir archivo para escritura.");
+    return fp;
+}
+
+void cerrarArchivo(FILE *fp) {
+    if (fp != NULL) fclose(fp);
+    else manejarError("El archivo ya está cerrado o es NULL.");
+}
+
+void leerDesdeArchivo(FILE *fp, const char *formato, ...) {
+    if (fp == NULL) {
+        printf("Error: Archivo no abierto para lectura.\n");
+        return;
+    }
+
+    va_list args;
+    va_start(args, formato);
+    if (fscanf(fp, formato, args) == EOF) {
+        if (feof(fp)) {
+            printf("Fin del archivo alcanzado.\n");
+        } else {
+            printf("Error al leer el archivo.\n");
+        }
+    }
+    va_end(args);
+}
+
+void escribirEnArchivo(FILE *fp, const char *formato, ...) {
+    va_list args;
+    if (fp == NULL) {
+        manejarError("Archivo no abierto para escritura.");
+        return;
+    }
+    va_start(args, formato);
+    if (vfprintf(fp, formato, args) < 0) manejarError("Error al escribir en el archivo.");
+    va_end(args);
+}
+
+void escribirEnBuffer(char *buffer, size_t tamanoBuffer, const char *formato, ...) {
+    va_list args;
+    va_start(args, formato);
+    vsnprintf(buffer, tamanoBuffer, formato, args);
+    va_end(args);
+}
+
+int FinalArchivo(FILE *archivo) {
+    return feof(archivo);
+}
+
+void eliminarArchivo(const char *nombreArchivo) {
+    if (remove(nombreArchivo) == 0) printf("Archivo eliminado correctamente.\n");
+    else manejarError("Error al eliminar el archivo");
+}
+
+void renombrarArchivo(const char *nombreActual, const char *nuevoNombre) {
+    if (rename(nombreActual, nuevoNombre) == 0) printf("Archivo renombrado correctamente.\n");
+    else manejarError("Error al renombrar el archivo");
+}
+
+void manejarError(const char *mensaje) {
+    perror(mensaje);
+}
+  
