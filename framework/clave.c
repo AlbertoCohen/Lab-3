533979ac00936ba892c587a053663cb2b31bb706
Alberto Cohen <143648228+AlbertoCohen@users.noreply.github.com>
2024-09-15 19:21:08 -0300

    clave.c

--- framework/clave.c
+++ framework/clave.c
@@ -1,14 +1,13 @@
 #include <sys/ipc.h>
 #include "clave.h"
 #include "def.h"
-#include "stdlib.h"
-key_t creo_clave(void) {
-key_t clave;
-clave = ftok ("/bin/ls",CLAVE_BASE);
-if (clave == (key_t)-1)
-{
-printf("No puedo conseguir clave semáforo, mem compartida, etc.\n");
-exit(0);
-}
-return clave;
+#include <stdio.h>
+key_t creo_clave(){
+    key_t clave;
+    clave = ftok("/bin/ls", CLAVE_BASE);
+    if (clave == (key_t)-1){
+        printf("No puedo conseguir clave semáforo, mem compartida, etc.\n");
+        exit(0);
+    }
+    return clave;
 }
