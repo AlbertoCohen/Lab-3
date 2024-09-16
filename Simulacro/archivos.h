91628225e4aacc7b876ed1228fe51400a454fa09
Alberto Cohen <143648228+AlbertoCohen@users.noreply.github.com>
2024-09-15 22:56:20 -0300

    Update archivos 2.h

--- Simulacro/archivos.h
+++ Simulacro/archivos.h
@@ -4,18 +4,20 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+FILE *abrirArchivo(const char *nombre, const char *modo);
 FILE* abrirArchivoW(const char *nombre);
 FILE* abrirArchivoR(const char *nombre);
 FILE* abrirArchivoA(const char *nombre);
 FILE* abrirArchivoWPlus(const char *nombre);
 FILE* abrirArchivoRPlus(const char *nombre);
 FILE* abrirArchivoAPlus(const char *nombre);
-void cerrarArchivo(FILE *fp);
 
+void cerrarArchivo(FILE *fp);
+void leerTodoElArchivo(FILE *fp);
+void leerDesdeArchivo(const char *nombreArchivo, const char *formato, int num_vars, ...);
 void escribirEnArchivo(FILE *fp, const char *formato, ...);
 void escribirEnBuffer(char *buffer, size_t tamanoBuffer, const char *formato, ...);
 int FinalArchivo(FILE *archivo);
-void leerTodoElArchivo(FILE *fp);
 void eliminarArchivo(const char *nombreArchivo);
 void renombrarArchivo(const char *nombreActual, const char *nuevoNombre);
 void manejarError(const char *mensaje);
